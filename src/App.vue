524989077823a08c3c76a64666f56e4c6c1c8d4b
David de los Santos Boix <ddlsb@hotmail.com>
2022-04-12 17:56:06 +0200

    Enhance a11y

--- src/App.vue
+++ src/App.vue
@@ -1,150 +1,165 @@
 <template>
-  <h1 class="text-3xl text-center font-bold text-gris">Find your next Game Pass Experience!</h1>
-  <div class="hidden md:block text-center mb-3 text-xs">
-    <p>Games information extracted from <a class="text-xbox" href="https://www.xbox.com/en-US/xbox-game-pass/games" target="_blank" rel="noopener">Game Pass Library</a>.</p>
-    <p>Duration information extracted from <a class="text-xbox" href="https://howlongtobeat.com/" target="_blank" rel="noopener">HowLongToBeat</a> (only Main Story).</p>
-  </div>
+  <header>
+    <h1 class="text-3xl text-center font-bold text-gris">Find your next Game Pass Experience!</h1>
+    <div class="hidden md:block text-center mb-3 text-xs">
+      <p>Games information extracted from <a class="text-xbox" href="https://www.xbox.com/en-US/xbox-game-pass/games" target="_blank" rel="noopener">Game Pass Library</a>.</p>
+      <p>Duration information extracted from <a class="text-xbox" href="https://howlongtobeat.com/" target="_blank" rel="noopener">HowLongToBeat</a> (only Main Story).</p>
+    </div>
 
-  <div class="flex bg-white w-full flex-wrap md:flex-inline justify-center pb-4">
-    <div class="w-full md:w-2/3">
-      <div class="flex flex-wrap mb-3">
-        <input
-          v-model="searchTerm"
-          type="text"
-          class="w-full md:w-3/4 px-3 py-1 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md"
-          placeholder="Search for your game!"
-        />
+    <div class="flex bg-white w-full flex-wrap md:flex-inline justify-center pb-4">
+      <div class="w-full md:w-2/3">
+        <div class="flex flex-wrap mb-3">
+          <input
+            v-model="searchTerm"
+            type="text"
+            class="w-full md:w-3/4 px-3 py-1 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md"
+            placeholder="Search for your game!"
+          />
 
-        <select v-model="selectedCategory" class="w-full md:w-1/4 px-2 py-1 border border-gray-300 rounded-md" :class="selectedCategory == '' ? 'text-gray-500' : 'text-gray-900'">
-          <option value="" class="text-gray-500">Filter by category!</option>
-          <option class="text-gray-900" v-for="category in categories" :key="category" :value="category">{{ category }}</option>
-        </select>
-      </div>
-
-      <div class="flex flex-wrap md:flex-inline justify-around">
-        <!-- PLATFORM & ORDERING -->
-        <div class="mb-2 md:mb-0">
-          <div class="flex flex-inline justify-center">
-            <button class="mx-2 text-white font-bold w-12 rounded-lg p-2 h-12" @click="addPlatform('PC')" :class="platforms.includes('PC') ? 'bg-xbox' : 'bg-gris'">
-              <IconPC />
-            </button>
-
-            <button class="mx-2 text-white font-bold w-12 rounded-lg p-2 h-12" @click="addPlatform('XBOX')" :class="platforms.includes('XBOX') ? 'bg-xbox' : 'bg-gris'">
-              <IconXbox />
-            </button>
-
-            <button class="mx-2 text-white font-bold w-12 rounded-lg p-2 h-12" @click="addPlatform('XCLOUD')" :class="platforms.includes('XCLOUD') ? 'bg-xbox' : 'bg-gris'">
-              <IconXCloud />
-            </button>
-          </div>
-
-          <div class="flex flex-inline justify-center space-x-2 mt-2 text-white font-semibold">
-            <button @click="sortByDuration = true" class="rounded-md px-2 py-1" :class="sortByDuration ? 'bg-xbox' : 'bg-gris'">Shortest</button>
-            <button @click="sortByDuration = false" class="rounded-md px-2 py-1" :class="sortByDuration ? 'bg-gris' : 'bg-xbox'">Newest</button>
-          </div>
+          <select
+            aria-label="Filter by category"
+            v-model="selectedCategory"
+            class="w-full md:w-1/4 px-2 py-1 border border-gray-300 rounded-md"
+            :class="selectedCategory == '' ? 'text-gray-500' : 'text-gray-900'"
+          >
+            <option value="" class="text-gray-500">Filter by category!</option>
+            <option class="text-gray-900" v-for="category in categories" :key="category" :value="category">{{ category }}</option>
+          </select>
         </div>
-        <!-- END PLATFORM & ORDERING -->
 
-        <!-- COOP & MULTI -->
-        <div class="mb-2 md:mb-0 space-y-2">
-          <div class="flex flex-inline justify-center">
-            <p class="my-auto font-bold text-gris mr-2 w-12 text-right">Local</p>
-            <button class="mx-1 text-white font-bold rounded-lg p-2 h-10 w-10" @click="localCoop = !localCoop" :class="localCoop ? 'bg-xbox' : 'bg-gris'" title="Local Cooperative">
-              <IconCoop />
-            </button>
+        <div class="flex flex-wrap md:flex-inline justify-around">
+          <!-- PLATFORM & ORDERING -->
+          <div class="mb-2 md:mb-0">
+            <div class="flex flex-inline justify-center">
+              <button
+                aria-label="Filter by PC game"
+                class="mx-2 text-white font-bold w-12 rounded-lg p-2 h-12"
+                @click="addPlatform('PC')"
+                :class="platforms.includes('PC') ? 'bg-xbox' : 'bg-gris'"
+              >
+                <IconPC />
+              </button>
 
-            <button
-              class="mx-1 text-white font-bold rounded-lg p-2 h-10 w-10"
-              @click="localMultiplayer = !localMultiplayer"
-              :class="localMultiplayer ? 'bg-xbox' : 'bg-gris'"
-              title="Multiplayer Local"
-            >
-              <IconMultiplayer />
-            </button>
+              <button
+                aria-label="Filter by Console game"
+                class="mx-2 text-white font-bold w-12 rounded-lg p-2 h-12"
+                @click="addPlatform('XBOX')"
+                :class="platforms.includes('XBOX') ? 'bg-xbox' : 'bg-gris'"
+              >
+                <IconXbox />
+              </button>
+
+              <button
+                aria-label="Filter by Cloud game"
+                class="mx-2 text-white font-bold w-12 rounded-lg p-2 h-12"
+                @click="addPlatform('XCLOUD')"
+                :class="platforms.includes('XCLOUD') ? 'bg-xbox' : 'bg-gris'"
+              >
+                <IconXCloud />
+              </button>
+            </div>
+
+            <div class="flex flex-inline justify-center space-x-2 mt-2 text-white font-semibold">
+              <button @click="sortByDuration = true" class="rounded-md px-2 py-1" :class="sortByDuration ? 'bg-xbox' : 'bg-gris'">Shortest</button>
+              <button @click="sortByDuration = false" class="rounded-md px-2 py-1" :class="sortByDuration ? 'bg-gris' : 'bg-xbox'">Newest</button>
+            </div>
           </div>
+          <!-- END PLATFORM & ORDERING -->
 
-          <div class="flex flex-inline justify-center">
-            <p class="my-auto font-bold text-gris mr-2 w-12">Online</p>
-            <button
-              class="mx-1 text-white font-bold rounded-lg p-2 h-10 w-10"
-              @click="onlineCoop = !onlineCoop"
-              :class="onlineCoop ? 'bg-xbox' : 'bg-gris'"
-              title="Cooperativo Online"
-            >
-              <IconCoop />
-            </button>
+          <!-- COOP & MULTI -->
+          <div class="mb-2 md:mb-0 space-y-2">
+            <div class="flex flex-inline justify-center">
+              <p class="my-auto font-bold text-gris mr-2 w-12 text-right">Local</p>
+              <button
+                class="mx-1 text-white font-bold rounded-lg p-2 h-10 w-10"
+                @click="localCoop = !localCoop"
+                :class="localCoop ? 'bg-xbox' : 'bg-gris'"
+                title="Local Cooperative"
+              >
+                <IconCoop />
+              </button>
 
-            <button
-              class="mx-1 text-white font-bold rounded-lg p-2 h-10 w-10"
-              @click="onlineMultiplayer = !onlineMultiplayer"
-              :class="onlineMultiplayer ? 'bg-xbox' : 'bg-gris'"
-              title="Online Multiplayer"
-            >
-              <IconMultiplayer />
-            </button>
+              <button
+                class="mx-1 text-white font-bold rounded-lg p-2 h-10 w-10"
+                @click="localMultiplayer = !localMultiplayer"
+                :class="localMultiplayer ? 'bg-xbox' : 'bg-gris'"
+                title="Local Multiplayer"
+              >
+                <IconMultiplayer />
+              </button>
+            </div>
+
+            <div class="flex flex-inline justify-center">
+              <p class="my-auto font-bold text-gris mr-2 w-12">Online</p>
+              <button
+                class="mx-1 text-white font-bold rounded-lg p-2 h-10 w-10"
+                @click="onlineCoop = !onlineCoop"
+                :class="onlineCoop ? 'bg-xbox' : 'bg-gris'"
+                title="Online Cooperative"
+              >
+                <IconCoop />
+              </button>
+
+              <button
+                class="mx-1 text-white font-bold rounded-lg p-2 h-10 w-10"
+                @click="onlineMultiplayer = !onlineMultiplayer"
+                :class="onlineMultiplayer ? 'bg-xbox' : 'bg-gris'"
+                title="Online Multiplayer"
+              >
+                <IconMultiplayer />
+              </button>
+            </div>
           </div>
+          <!-- COOP & MULTI -->
+
+          <!-- COMING & LEAVING -->
+          <div>
+            <div class="flex flex-inline justify-center space-x-2 mt-2 text-white font-semibold">
+              <button @click="addUniqueFilter('comingSoonPC')" class="rounded-md px-2 py-1" :class="uniqueFilter == 'comingSoonPC' ? 'bg-xbox' : 'bg-gris'">
+                <div class="flex flex-inline">
+                  <div class="h-6 w-6">
+                    <IconPC />
+                  </div>
+                  <span class="ml-1">Upcoming</span>
+                </div>
+              </button>
+              <button @click="addUniqueFilter('leavingSoonPC')" class="rounded-md px-2 py-1" :class="uniqueFilter == 'leavingSoonPC' ? 'bg-xbox' : 'bg-gris'">
+                <div class="flex flex-inline">
+                  <div class="h-6 w-6">
+                    <IconPC />
+                  </div>
+                  <span class="ml-1">Leaving</span>
+                </div>
+              </button>
+            </div>
+            <div class="flex flex-inline justify-center space-x-2 mt-2 text-white font-semibold">
+              <button @click="addUniqueFilter('comingSoonConsole')" class="rounded-md px-2 py-1" :class="uniqueFilter == 'comingSoonConsole' ? 'bg-xbox' : 'bg-gris'">
+                <div class="flex flex-inline">
+                  <div class="h-6 w-6">
+                    <IconXbox />
+                  </div>
+                  <span class="ml-1">Upcoming</span>
+                </div>
+              </button>
+              <button @click="addUniqueFilter('leavingSoonConsole')" class="rounded-md px-2 py-1" :class="uniqueFilter == 'leavingSoonConsole' ? 'bg-xbox' : 'bg-gris'">
+                <div class="flex flex-inline">
+                  <div class="h-6 w-6">
+                    <IconXbox />
+                  </div>
+                  <span class="ml-1">Leaving</span>
+                </div>
+              </button>
+            </div>
+          </div>
+          <!-- END COMING & LEAVING -->
         </div>
-        <!-- COOP & MULTI -->
-
-        <!-- COMING & LEAVING -->
-        <div>
-          <div class="flex flex-inline justify-center space-x-2 mt-2 text-white font-semibold">
-            <button @click="addUniqueFilter('comingSoonPC')" class="rounded-md px-2 py-1" :class="uniqueFilter == 'comingSoonPC' ? 'bg-xbox' : 'bg-gris'">
-              <div class="flex flex-inline">
-                <div class="h-6 w-6">
-                  <IconPC />
-                </div>
-                <span class="ml-1">Upcoming</span>
-              </div>
-            </button>
-            <button @click="addUniqueFilter('leavingSoonPC')" class="rounded-md px-2 py-1" :class="uniqueFilter == 'leavingSoonPC' ? 'bg-xbox' : 'bg-gris'">
-              <div class="flex flex-inline">
-                <div class="h-6 w-6">
-                  <IconPC />
-                </div>
-                <span class="ml-1">Leaving</span>
-              </div>
-            </button>
-          </div>
-          <div class="flex flex-inline justify-center space-x-2 mt-2 text-white font-semibold">
-            <button @click="addUniqueFilter('comingSoonConsole')" class="rounded-md px-2 py-1" :class="uniqueFilter == 'comingSoonConsole' ? 'bg-xbox' : 'bg-gris'">
-              <div class="flex flex-inline">
-                <div class="h-6 w-6">
-                  <IconXbox />
-                </div>
-                <span class="ml-1">Upcoming</span>
-              </div>
-            </button>
-            <button @click="addUniqueFilter('leavingSoonConsole')" class="rounded-md px-2 py-1" :class="uniqueFilter == 'leavingSoonConsole' ? 'bg-xbox' : 'bg-gris'">
-              <div class="flex flex-inline">
-                <div class="h-6 w-6">
-                  <IconXbox />
-                </div>
-                <span class="ml-1">Leaving</span>
-              </div>
-            </button>
-          </div>
-        </div>
-        <!-- END COMING & LEAVING -->
       </div>
     </div>
-  </div>
+  </header>
 
-  <div class="bg-gris">
-    <main class="container mx-auto px-3">
+  <main class="bg-gris">
+    <div class="container mx-auto px-3">
       <div class="flex flex-wrap justify-center items-center">
-        <div class="text-center text-white mt-3 font-semibold">
-          <p>
-            Total Games: <span class="font-light">{{ filterGames().length }}</span>
-          </p>
-          <p>
-            Total PlayTime:
-            <span class="font-light">
-              {{ playTime() }} hours <span class="text-xs">(~ {{ Math.round(playTime() / 24) }} days)</span></span
-            >
-          </p>
-        </div>
-
         <Pagination :totalPages="totalPages" :currentPage="currentPage" @changedPage="(newPage) => (currentPage = newPage)" />
 
         <div class="flex flex-wrap w-full" role="list">
@@ -219,20 +234,20 @@
                     <div class="font-bold text-md">{{ game.title }}</div>
                     <div class="text-sm text-gray-600">{{ game.category }}</div>
 
-                    <div class="text-white flex text-xs -mx-1.5 justify-center">
+                    <div class="text-white font-semibold flex text-xs -mx-1.5 justify-center">
                       <div class="w-1/2">
                         <div v-if="game.localCoop || game.localMultiplayer" class="mt-1">
                           <p class="text-gris text-md font-bold pl-2">Local</p>
                           <div class="flex space-x-0.5">
-                            <div v-if="game.localCoop" class="flex bg-xbox pl-1 pr-2 py-1 h-8 rounded-full w-max"  title="Local Cooperative">
-                              <div class="h-5 my-auto">
+                            <div v-if="game.localCoop" class="flex bg-xbox pl-1 pr-2 py-1 h-8 rounded-full w-max" title="Local Cooperative">
+                              <div class="h-4 my-auto">
                                 <IconCoop />
                               </div>
                               <div class="my-auto pl-1" v-if="game.localCoop.min && game.localCoop.max">{{ game.localCoop.min }}-{{ game.localCoop.max }}</div>
                             </div>
 
-                            <div v-if="game.localMultiplayer" class="flex bg-xbox pl-1 pr-2 py-1 h-8 rounded-full w-max"  title="Multiplayer local">
-                              <div class="h-5 my-auto">
+                            <div v-if="game.localMultiplayer" class="flex bg-xbox pl-1 pr-2 py-1 h-8 rounded-full w-max" title="Local Multiplayer">
+                              <div class="h-4 my-auto">
                                 <IconMultiplayer />
                               </div>
                               <div class="my-auto pl-1" v-if="game.localMultiplayer.min && game.localMultiplayer.max">
@@ -247,15 +262,15 @@
                         <div v-if="game.localCoop || game.localMultiplayer" class="mt-1">
                           <p class="text-gris text-md font-bold pl-2">Online</p>
                           <div class="flex space-x-0.5">
-                            <div v-if="game.onlineCoop" class="flex bg-xbox pl-1 pr-2 py-1 h-8 rounded-full w-max"  title="Cooperativo Online">
-                              <div class="h-5 my-auto">
+                            <div v-if="game.onlineCoop" class="flex bg-xbox pl-1 pr-2 py-1 h-8 rounded-full w-max" title="Online Cooperative">
+                              <div class="h-4 my-auto">
                                 <IconCoop />
                               </div>
                               <div class="my-auto pl-1" v-if="game.onlineCoop.min && game.onlineCoop.max">{{ game.onlineCoop.min }}-{{ game.onlineCoop.max }}</div>
                             </div>
 
-                            <div v-if="game.onlineMultiplayer" class="flex bg-xbox pl-1 pr-2 py-1 h-8 rounded-full w-max"  title="Online Multiplayer">
-                              <div class="h-5 my-auto">
+                            <div v-if="game.onlineMultiplayer" class="flex bg-xbox pl-1 pr-2 py-1 h-8 rounded-full w-max" title="Online Multiplayer">
+                              <div class="h-4 my-auto">
                                 <IconMultiplayer />
                               </div>
                               <div class="my-auto pl-1" v-if="game.onlineMultiplayer.min && game.onlineMultiplayer.max">
@@ -275,8 +290,8 @@
 
         <Pagination :totalPages="totalPages" :currentPage="currentPage" @changedPage="(newPage) => (currentPage = newPage)" />
       </div>
-    </main>
-  </div>
+    </div>
+  </main>
 </template>
 
 <script>
